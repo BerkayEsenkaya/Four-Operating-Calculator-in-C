3ad7567a368745ff693aa324806b9bf4b3ba2a12
BerkayEsenkaya <64868082+BerkayEsenkaya@users.noreply.github.com>
2020-11-25 15:14:27 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,17 +3,19 @@
 #include<conio.h>
 
 int main(){
-	float sayi1,sayi2,sonuc=0;
+	float sayi1,sayi2,sonuc=0,i,yertutma;
 	char islem;
 	scanf("%f",&sayi1);
 	sonuc = sayi1;
 	while(islem != '='){
 		
 		scanf(" %c",&islem);
+		if(islem !='!'){
 		
 		scanf("%f",&sayi2);
+	   }
 		
-		switch(islem){
+	    switch(islem){
 			
 			case '+':
 			    sonuc += sayi2;
@@ -33,18 +35,28 @@ int main(){
 			    sonuc /= sayi2;
 			    
 			    break;
-	
-	}   
+		        case '^':
+		    	    if(sonuc<0 && sayi2<1 && sayi2>0){
+		    		printf("undefined\n ");
+				}
+		    	    else{
+				sonuc=pow(sonuc, sayi2);
+			       }
+				break;
+			case '!':
+				yertutma=sonuc;
+				sonuc=1;
+			    for(i=1;i<=yertutma;i++){
+			       sonuc*=i;	
+				}
+				
+			    
+				break;	
+    	}  
 	    
 		printf("= %.2f",sonuc);
-		sayi2=0;
-		
-		
+		sayi2=0;	
 	}
 	
-	
-	
-	
-	
-	return 0;
-}
+	 return 0;
+			}
